73588017d74bef811d34d0eac303f04e204ab550
kobe <417125111@qq.com>
2017-07-04 08:43:50 +0800

    risume

---
+++ js/index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Administrator on 2017/5/24.
+ */
+
+//window.onload = function () {
+//    function onresize(){
+//        var w = document.documentElement.clientWidth;
+//        w = w>768 ?768:w;
+//        var html = document.querySelector("html");
+//        html.style.fontSize = w/20 + "px";
+//    }
+
+    //
+    //window.onresize = function () {
+    //    onresize();
+    //}
+    //第二屏，第一个动画。
+
+
+
+
+
+
+
